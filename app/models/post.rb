018f731ba74eb2dcbc8309e14e42f7facda5e711
drewsview34 <drewsview34@outlook.com>
2021-08-05 11:54:07 -0700

    added validations to Post, User, and Tag model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ActiveRecord::Base
     has_many :tags
     has_many :users, through: :tags
 
+    validates :name, presence: true
+    validates :content, presence: true
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ApplicationRecord
     has_many :posts
     has_many :users, through: :posts
 
+    validates :name, uniqueness: true
+
 end
